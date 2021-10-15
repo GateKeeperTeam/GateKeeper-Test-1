bb438a21e299a56a560b45759fb1b802a702f24b
Thomas Lyons <lyonsta@mail.uc.edu>
2021-10-15 17:24:32 -0400

    Initial Commit

---
+++ GateKeeper Test 1/GateKeeper_Test_1App.swift
@@ -0,0 +1,17 @@
+//
+//  GateKeeper_Test_1App.swift
+//  GateKeeper Test 1
+//
+//  Created by Thomas Lyons on 10/15/21.
+//
+
+import SwiftUI
+
+@main
+struct GateKeeper_Test_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
